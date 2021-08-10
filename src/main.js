e273cf8721442e1d3dc92ef1eb3d12faa28b15e4
mil <1363944642@qq.com>
2021-08-10 16:12:22 +0800

    创建web服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-const greeting = 'hello ~';
-console.log(greeting);
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('hello ~')
+  response.end();
+});
+
+server.listen(3000, () => {
+  console.log('🚀 服务已启动! ');
+})
