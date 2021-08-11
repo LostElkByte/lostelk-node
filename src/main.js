29d510642ccf8c7152927fef795d28234a69ea14
mil <1363944642@qq.com>
2021-08-11 15:24:09 +0800

    应用框架

--- src/main.js
+++ src/main.js
@@ -5,3 +5,32 @@ const port = 3000;
 app.listen(port, () => {
   console.log('🚀 服务已启动!');
 })
+
+app.get('/', (request, response) => {
+  response.send('你好');
+});
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山,苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '关山月1',
+    content: '明月出天山,苍茫云海间1'
+  }
+]
+
+app.get('/posts', (request, response) => {
+  response.send(data)
+})
+
+app.get('/posts/:postId', (request, response) => {
+  const { postId } = request.params;
+
+  const posts = data.filter(item => item.id == postId)
+
+  response.send(posts[0])
+})
