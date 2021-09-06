0cc148ecfe063b4074392ea09d31eae0a0821235
mil <1363944642@qq.com>
2021-09-06 15:00:26 +0800

    上传文件

---
+++ src/file/file.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction } from 'express'
+import _ from 'lodash'
+import { createFile, findFileById } from './file.service'
+
+/**
+* 上传文件
+*/
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 当前用户
+  const { id: userId } = request.user
+
+  // 所属内容
+  const postId = parseInt(request.query.post as string, 10)
+
+  // 文件信息
+  const fileInfo = _.pick(request.file, [
+    'originalname',
+    'mimetype',
+    'filename',
+    'size'
+  ])
+
+  try {
+    // 保存文件信息
+    const data = await createFile({
+      ...fileInfo,
+      userId,
+      postId,
+    })
+
+    // 做出响应
+    response.status(201).send(data)
+  } catch (error) {
+    next(error)
+  }
+}
+
+/**
+* 文件服务
+*/
+export const serve = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 从地址参数里得到文件 ID
+  const { fileId } = request.params
+
+  try {
+    // 查找文件信息
+    const file = await findFileById(parseInt(fileId, 10))
+
+    // 做出响应
+    response.sendFile(file.filename, {
+      root: 'uploads',
+      headers: {
+        'Content-Type': file.mimetype
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
