ada52099a77851bd36c408035239148c65d6e303
mil <1363944642@qq.com>
2021-09-16 09:53:02 +0800

    暂存

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,7 @@ import userRouter from '../user/user.router'
 import authRouter from '../auth/auth.router'
 import fileRouter from '../file/file.router'
 import tagRouter from '../tag/tag.router'
+import commentRouter from '../comment/comment.router'
 import { defaultErrorHandler } from './app.middleware'
 
 /**
@@ -24,7 +25,8 @@ app.use(
   userRouter,
   authRouter,
   fileRouter,
-  tagRouter
+  tagRouter,
+  commentRouter
 );
 
 /**
