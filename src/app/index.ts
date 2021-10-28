e402d01aaf5aa6bedbf886ddf47cdecbb566ed64
mil <1363944642@qq.com>
2021-10-28 15:15:09 +0800

    定义上传头像接口

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,7 +5,7 @@ import authRouter from '../auth/auth.router'
 import fileRouter from '../file/file.router'
 import tagRouter from '../tag/tag.router'
 import commentRouter from '../comment/comment.router'
-import avatarRouter from '../comment/comment.router'
+import avatarRouter from '../avatar/avatar.router'
 import { defaultErrorHandler } from './app.middleware'
 
 /**
--- src/avatar/avatar.middleware.ts
+++ src/avatar/avatar.middleware.ts
@@ -1,1 +1,21 @@
 import { Request, Response, NextFunction } from 'express'
+import multer from 'multer'
+import { fileFilter } from '../file/file.middleware'
+
+/**
+ * 文件过滤器
+ */
+const avatarUploadFiler = fileFilter(['image/png', 'image/jpg', 'image/jpeg'])
+
+/**
+ * 创建一个 Multer
+ */
+const avatarUpload = multer({
+  dest: 'uploads/avatar',
+  fileFilter: avatarUploadFiler
+})
+
+/**
+ * 文件拦截器
+ */
+export const avatarInterceptor = avatarUpload.single('avatar')
