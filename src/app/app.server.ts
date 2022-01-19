8191230e453f0a49839e2cb9af7c65aa0fa201ce
mil <1363944642@qq.com>
2022-01-19 15:15:33 +0800

    创建 Socket.io 实时服务器

--- src/app/app.server.ts
+++ src/app/app.server.ts
@@ -1,10 +1,22 @@
 import http from 'http'
+import { Server } from 'socket.io'
+import { ALLOW_ORIGIN } from './app.config'
 import app from './index'
 
 /**
  * HTTP 服务器
  */
 const httpServer = http.createServer(app)
+
+/**
+ * IO 服务器
+ */
+export const socketIoServer = new Server(httpServer, {
+  cors: {
+    origin: ALLOW_ORIGIN,
+    allowedHeaders: ['X-Total-Count']
+  }
+})
 
 /**
  * 默认导出
