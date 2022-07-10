1f51c1d3a096fa645bfdf33433b2bce496b8c802
mil <1363944642@qq.com>
2022-07-10 19:52:15 +0800

    修改主色提取质量为1

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,22 +1,19 @@
-import path from 'path'
-import fs from 'fs'
-import Jimp from 'jimp'
-import ColorThief from 'colorthief'
-import { connection } from '../app/database/mysql'
-import { FileModel } from './file.model'
-import { colord, extend } from "colord";
-import namesPlugin from "colord/plugins/names";
-import _ from 'lodash'
+import path from 'path';
+import fs from 'fs';
+import Jimp from 'jimp';
+import ColorThief from 'colorthief';
+import { connection } from '../app/database/mysql';
+import { FileModel } from './file.model';
+import { colord, extend } from 'colord';
+import namesPlugin from 'colord/plugins/names';
+import _ from 'lodash';
 
 extend([namesPlugin]);
 
-
 /**
-* 存储文件信息
-*/
-export const createFile = async (
-  file: FileModel
-) => {
+ * 存储文件信息
+ */
+export const createFile = async (file: FileModel) => {
   // 准备查询
   const statement = `
     INSERT INTO file
@@ -24,18 +21,16 @@ export const createFile = async (
   `;
 
   // 执行查询
-  const [data] = await connection.promise().query(statement, file)
+  const [data] = await connection.promise().query(statement, file);
 
   // 提供数据
   return data;
-}
+};
 
 /**
  * 按 ID 查找文件
  */
-export const findFileById = async (
-  fileId: number
-) => {
+export const findFileById = async (fileId: number) => {
   // 准备查询
   const statement = `
     SELECT * FROM file
@@ -43,38 +38,47 @@ export const findFileById = async (
   `;
 
   // 执行查询
-  const [data] = await connection.promise().query(statement, fileId)
+  const [data] = await connection.promise().query(statement, fileId);
 
   // 提供数据
-  return data[0]
-}
+  return data[0];
+};
 
 /**
-* 添加图像尺寸
-*/
-export const imageResizer = async (
-  image: Jimp, file: Express.Multer.File
-) => {
+ * 添加图像尺寸
+ */
+export const imageResizer = async (image: Jimp, file: Express.Multer.File) => {
   // 图像尺寸
-  let width: number
+  let width: number;
   if (image['_exif']) {
-    width = image['_exif'].imageSize.width
+    width = image['_exif'].imageSize.width;
   } else {
-    width = image['bitmap'].width
+    width = image['bitmap'].width;
   }
 
   // 文件路径
-  const largeFilePath = path.join(file.destination, 'resized', `large-${file.filename}`)
-  const mediumFilePath = path.join(file.destination, 'resized', `medium-${file.filename}`)
-  const thumbnailFilePath = path.join(file.destination, 'resized', `thumbnail-${file.filename}`)
-
+  const largeFilePath = path.join(
+    file.destination,
+    'resized',
+    `large-${file.filename}`,
+  );
+  const mediumFilePath = path.join(
+    file.destination,
+    'resized',
+    `medium-${file.filename}`,
+  );
+  const thumbnailFilePath = path.join(
+    file.destination,
+    'resized',
+    `thumbnail-${file.filename}`,
+  );
 
   // 大尺寸
   if (width > 1280) {
     image
       .resize(1280, Jimp.AUTO)
       .quality(100)
-      .write(`${largeFilePath}`)
+      .write(`${largeFilePath}`);
   }
 
   // 中等尺寸
@@ -82,7 +86,7 @@ export const imageResizer = async (
     image
       .resize(640, Jimp.AUTO)
       .quality(85)
-      .write(`${mediumFilePath}`)
+      .write(`${mediumFilePath}`);
   }
 
   // 缩略图
@@ -90,104 +94,134 @@ export const imageResizer = async (
     image
       .resize(320, Jimp.AUTO)
       .quality(20)
-      .write(`${thumbnailFilePath}`)
+      .write(`${thumbnailFilePath}`);
   }
-}
+};
 
 /**
  * 转换十六进制
- * @param r 
- * @param g 
- * @param b 
- * @returns 
+ * @param r
+ * @param g
+ * @param b
+ * @returns
  */
-export const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
-  const hex = x.toString(16)
-  return hex.length === 1 ? '0' + hex : hex
-}).join('')
-
+export const rgbToHex = (r, g, b) =>
+  '#' +
+  [r, g, b]
+    .map(x => {
+      const hex = x.toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    })
+    .join('');
 
 /**
  * 提取图片颜色
  */
 export const extractionColor = async (
-  image: Jimp, file: Express.Multer.File,
+  image: Jimp,
+  file: Express.Multer.File,
 ) => {
   // 图像尺寸
-  let width: number
+  let width: number;
   if (image['_exif']) {
-    width = image['_exif'].imageSize.width
+    width = image['_exif'].imageSize.width;
   } else {
-    width = image['bitmap'].width
+    width = image['bitmap'].width;
   }
 
   // 文件路径
-  const mediumFilePath = path.join(file.destination, 'resized', `medium-${file.filename}`)
+  const mediumFilePath = path.join(
+    file.destination,
+    'resized',
+    `medium-${file.filename}`,
+  );
 
   // RGB主色
-  let mainColor: any
+  let mainColor: any;
   // RGB调色板列表
-  let paletteColor: any
+  let paletteColor: any;
 
   if (width > 320) {
     // 提取图片RGV主色
-    await ColorThief.getColor(mediumFilePath)
-      .then(color => { mainColor = color })
-      .catch(err => { console.log(err) })
+    await ColorThief.getColor(mediumFilePath, 1)
+      .then(color => {
+        mainColor = color;
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     // 提取图片RGB副色
     await ColorThief.getPalette(mediumFilePath, 8)
-      .then(palette => { paletteColor = palette })
-      .catch(err => { console.log(err) })
+      .then(palette => {
+        paletteColor = palette;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   } else {
     // 提取图片RGB主色
-    await ColorThief.getColor(file.path)
-      .then(color => { mainColor = color })
-      .catch(err => { console.log(err) })
+    await ColorThief.getColor(file.path, 1)
+      .then(color => {
+        mainColor = color;
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     // 提取图片RGB副色
     await ColorThief.getPalette(file.path, 8)
-      .then(palette => { paletteColor = palette })
-      .catch(err => { console.log(err) })
+      .then(palette => {
+        paletteColor = palette;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   // 如果提取RGB主色失败
   if (!mainColor && paletteColor) {
-    mainColor = paletteColor[0]
+    mainColor = paletteColor[0];
   }
 
   // 主色HEX颜色
-  let mainHexColor: string
+  let mainHexColor: string;
   // 主色W3C颜色名
-  let mainColorName: string
+  let mainColorName: string;
 
   // 调色板HEX颜色列表
-  let paletteColornHexList = [] as Array<string>
+  let paletteColornHexList = [] as Array<string>;
   // 调色板W3C颜色名列表
-  let paletteColorNameList = [] as Array<string>
+  let paletteColorNameList = [] as Array<string>;
 
   // RBG转HEX
-  mainHexColor = rgbToHex(mainColor[0], mainColor[1], mainColor[2])
+  mainHexColor = rgbToHex(mainColor[0], mainColor[1], mainColor[2]);
   // 将HEX转为W3C颜色名
-  mainColorName = colord(mainHexColor).toName({ closest: true })
-
+  mainColorName = colord(mainHexColor).toName({ closest: true });
 
   // 将调色板RBG转为HEX
   for (const itemColor of paletteColor) {
     // RBG转HEX
-    const hexColor = rgbToHex(itemColor[0], itemColor[1], itemColor[2])
+    const hexColor = rgbToHex(itemColor[0], itemColor[1], itemColor[2]);
     // 将HEX转为W3C颜色名
-    const palettColorName = colord(hexColor).toName({ closest: true })
+    const palettColorName = colord(hexColor).toName({ closest: true });
     // PUSH进调色板HEX颜色列表
-    paletteColornHexList.push(hexColor)
+    paletteColornHexList.push(hexColor);
     // PUSH进调色板W3C颜色名列表
-    paletteColorNameList.push(palettColorName)
+    paletteColorNameList.push(palettColorName);
   }
   // 调色板W3C颜色名List去重
-  paletteColorNameList = Array.from(new Set(paletteColorNameList))
+  paletteColorNameList = Array.from(new Set(paletteColorNameList));
 
-  return { mainColor, paletteColor, mainColorName, paletteColorNameList, mainHexColor, paletteColornHexList, }
-}
+  return {
+    mainColor,
+    paletteColor,
+    mainColorName,
+    paletteColorNameList,
+    mainHexColor,
+    paletteColornHexList,
+  };
+};
 
 /**
  * 找出内容相关文件
@@ -203,19 +237,19 @@ export const getPostFiles = async (postId: number) => {
   `;
 
   // 执行
-  const [data] = await connection.promise().query(statement, postId)
+  const [data] = await connection.promise().query(statement, postId);
 
   // 提供数据
-  return data as any
-}
+  return data as any;
+};
 
 /**
  * 删除内容文件
  */
 export const deletePostFiles = async (files: Array<FileModel>) => {
   // 准备文件路径
-  const uploads = 'uploads/files'
-  const resized = [uploads, 'resized']
+  const uploads = 'uploads/files';
+  const resized = [uploads, 'resized'];
 
   files.map(file => {
     const filesToDelete = [
@@ -223,18 +257,18 @@ export const deletePostFiles = async (files: Array<FileModel>) => {
       [...resized, `thumbnail-${file.filename}`],
       [...resized, `medium-${file.filename}`],
       [...resized, `large-${file.filename}`],
-    ]
+    ];
 
     filesToDelete.map(item => {
-      const filePath = path.join(...item)
+      const filePath = path.join(...item);
 
       fs.stat(filePath, (error, stats) => {
         if (stats) {
           fs.unlink(filePath, error => {
-            if (error) throw error
-          })
+            if (error) throw error;
+          });
         }
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
